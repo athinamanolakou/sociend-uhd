f36f33738243201deac5051a68cd29d31a294bb8
niazi <niazi@uoguelph.ca>
2025-02-27 22:22:46 -0500

    Update housingService.test.ts frontend tests

--- frontend/src/services/housingService.test.ts
+++ frontend/src/services/housingService.test.ts
@@ -1,5 +1,5 @@
 // housingService.test.ts
-import { getAllHousingStartsCompletions } from './housingService';
+import {getHousingCompletionRatios, getHousingTotalStartsCompletions} from './housingService';
 import fetchMock from 'jest-fetch-mock';
 
 beforeAll(() => {
@@ -10,19 +10,47 @@ beforeEach(() => {
   fetchMock.resetMocks();
 });
 
-it('successfully fetches housing data', async () => {
-  const mockData = [{ id: 1, name: 'Test Data' }];
+//it('successfully fetches housing data', async () => {
+//  const mockData = [{ id: 1, name: 'Test Data' }];
+//  fetchMock.mockResponseOnce(JSON.stringify(mockData));
+
+//  const data = await getAllHousingStartsCompletions();
+//  expect(data).toEqual(mockData);
+//  expect(fetchMock.mock.calls.length).toEqual(1);
+//  expect(fetchMock.mock.calls[0][0]).toEqual('/api/housing/starts-completions/all');
+//});
+
+it("successfully fetches housing completion ratio data", async () => {
+  const mockData = [{id: 1, name: 'Test Data'}];
   fetchMock.mockResponseOnce(JSON.stringify(mockData));
 
-  const data = await getAllHousingStartsCompletions();
+  const data = await getHousingCompletionRatios();
   expect(data).toEqual(mockData);
   expect(fetchMock.mock.calls.length).toEqual(1);
-  expect(fetchMock.mock.calls[0][0]).toEqual('/api/housing/starts-completions/all');
+  expect(fetchMock.mock.calls[0][0]).toEqual('/api/housing/starts-completions/ratio');
 });
 
 it('handles fetch failure', async () => {
   fetchMock.mockReject(new Error('API failure'));
 
-  const data = await getAllHousingStartsCompletions();
+  const data = await getHousingCompletionRatios();
+  expect(data).toEqual([]);  // Check that it returns an empty array instead of throwing
+});
+
+
+it("Successfully fetches total housing starts and completions data", async () => {
+  const mockData = [{id: 1, name: 'Test Data'}];
+  fetchMock.mockResponseOnce(JSON.stringify(mockData));
+
+  const data = await getHousingTotalStartsCompletions();
+  expect(data).toEqual(mockData);
+  expect(fetchMock.mock.calls.length).toEqual(1);
+  expect(fetchMock.mock.calls[0][0]).toEqual('/api/housing/starts-completions/total');
+});
+
+it('handles fetch failure', async () => {
+  fetchMock.mockReject(new Error('API failure'));
+
+  const data = await getHousingTotalStartsCompletions();
   expect(data).toEqual([]);  // Check that it returns an empty array instead of throwing
 });
