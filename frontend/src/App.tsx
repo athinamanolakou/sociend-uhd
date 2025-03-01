71e002f4e0ce0bdebec2702d64854ec03fc215c8
rikho1229 <114449234+rikho1229@users.noreply.github.com>
2025-02-28 19:11:57 -0500

    added housing starts and completions graphs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HousingGraph from './components/HousingGraph';
+import HousingStartsGraph from './components/HousingStartsGraph';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <HousingGraph />
+      <HousingStartsGraph />
     </div>
   );
 };
