15d5cc2fa17ce9f2d2e892a58bf7e4516c471fc7
Shuby <mwahab@uoguelph.ca>
2025-03-14 12:01:03 -0400

    Comments removed

--- backend/src/main/java/com/example/cis4900/spring/template/housing/HousingService.java
+++ backend/src/main/java/com/example/cis4900/spring/template/housing/HousingService.java
@@ -1,11 +1,9 @@
 package com.example.cis4900.spring.template.housing;
 
-//import com.example.cis4900.spring.template.housing.models.HousingStartsCompletions;
 import java.util.List;
 import java.util.Map;
 
 public interface HousingService {
-    // List<HousingStartsCompletions> getAllHousingStartsCompletions();
 
     List<Map<String, Object>> getHousingTotals();
 
